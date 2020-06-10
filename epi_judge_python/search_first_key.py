911323a8740be49f332f8f74a380c5f9109b8d7b
Brian Ta <briant9957@gmail.com>
2020-06-09 23:39:05 -0500

    completed first occurrence of K

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,8 +4,21 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    
+    left, right = 0, len(A) - 1
+
+    while left <= right:
+        middle = left + (right-left) // 2
+        if A[middle] == k:
+            while middle > 0 and A[middle-1] == k:
+                middle -= 1
+            return middle
+        elif A[middle] < k:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return -1
 
 
 if __name__ == '__main__':
